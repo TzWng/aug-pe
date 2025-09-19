1351f535bbd88d0282ef5803fd4ad66797f49ee9
TzWng <91708166+TzWng@users.noreply.github.com>
2025-09-19 14:16:35 -0400

    Update downstream.sh

--- scripts/hf/yelp/downstream.sh
+++ scripts/hf/yelp/downstream.sh
@@ -1,5 +1,5 @@
 cls_batch_size=32
-result_folder="/content/drive/MyDrive/SecPE/synthetic_text/yelp_gpt4"
+result_folder="/content/drive/MyDrive/SecPE/synthetic_text/yelp_qwen7b"
 
 ### calculate acc 
 # bash scripts/hf/yelp/downstream.sh
