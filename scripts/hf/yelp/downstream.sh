9586d3de513b32f3812cfaff1cce2dc6506b4bf1
TzWng <91708166+TzWng@users.noreply.github.com>
2025-09-18 20:19:37 -0400

    Update downstream.sh

--- scripts/hf/yelp/downstream.sh
+++ scripts/hf/yelp/downstream.sh
@@ -1,6 +1,6 @@
 cls_batch_size=32
 result_folder="/content/drive/MyDrive/SecPE/synthetic_text/yelp_gpt2"
-file_name="cluster600_50p_prefixed.csv"
+file_name="cluster600_infty_prefixed.csv"
 
 ### calculate acc 
 # bash scripts/hf/yelp/downstream.sh
