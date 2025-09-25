4569cdbce2bfe337d6a15f42f0ed1e14d860837c
TzWng <91708166+TzWng@users.noreply.github.com>
2025-09-24 21:18:57 -0400

    Update downstream.sh

--- scripts/hf/yelp/downstream.sh
+++ scripts/hf/yelp/downstream.sh
@@ -6,13 +6,13 @@ result_folder="/content/drive/MyDrive/SecPE/results-yelp-random-all"
 num_train_epochs=5
 for seed in 42 4 6 8
 do
-for label in "label2"
+for label in "label1"
 do
 for  (( iter=${num_train_epochs}; iter>=0; iter-- ))
 do
 for file_name in "train.csv"
 do
-for folder in "qwen_800_0" "qwen_800_50"
+for folder in "qwen_800_10" "qwen_800_50"
 do
 train_file="${result_folder}/${folder}/${file_name}"
 if [ -e "$train_file" ]; then
