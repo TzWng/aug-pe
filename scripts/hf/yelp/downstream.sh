41aad704afb393f4bb58edbe0b051a3cf6f726ed
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-03 04:33:14 -0500

    Update downstream.sh

--- scripts/hf/yelp/downstream.sh
+++ scripts/hf/yelp/downstream.sh
@@ -18,7 +18,7 @@ train_file="${result_folder}/${method}_${privacy}_prefixed.csv"
 if [ -e "$train_file" ]; then
     echo "$train_file does exist."
 
-    output_dir=${result_folder}/downstream/${label}_seed${seed}_${privacy}/
+    output_dir=${result_folder}/downstream/${label}_seed${seed}_${method}_${privacy}p/
     if [ -e "${output_dir}test_${num_train_epochs}.0_results.json" ]; then
         echo "${output_dir}test_${num_train_epochs}.0_results.json  does exist. -- SKIP running classification"
     else
