ac31236a1be121673d52547f0aba223f21933172
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-03 05:55:10 -0500

    Update downstream.sh

--- scripts/hf/yelp/downstream.sh
+++ scripts/hf/yelp/downstream.sh
@@ -10,9 +10,9 @@ for label in "label2"
 do
 for  (( iter=${num_train_epochs}; iter>=0; iter-- ))
 do
-for privacy in 50
+for privacy in 0
 do
-for method in "mugdp" "secpe600"
+for method in "mugdp"
 do
 train_file="${result_folder}/${method}_${privacy}_prefixed.csv"
 if [ -e "$train_file" ]; then
