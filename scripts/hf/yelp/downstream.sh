0432d7487a16e82252dc7c88712e16970869700f
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-07 22:30:29 -0500

    Update downstream.sh

--- scripts/hf/yelp/downstream.sh
+++ scripts/hf/yelp/downstream.sh
@@ -6,11 +6,11 @@ result_folder="/content/drive/MyDrive/SecPE/yelp_pii_diff"
 num_train_epochs=5
 for seed in 3
 do
-for label in "label2"
+for label in "label1"
 do
 for  (( iter=${num_train_epochs}; iter>=0; iter-- ))
 do
-for privacy in 2
+for privacy in 50
 do
 for method in "secpe600"
 do
