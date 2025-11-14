5b8bb1cc16ee161bb0e961220cec8c1b58fc57c0
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-14 16:15:31 -0500

    Update downstream.sh

--- scripts/hf/yelp/downstream.sh
+++ scripts/hf/yelp/downstream.sh
@@ -4,7 +4,7 @@ result_folder="/content/drive/MyDrive/SecPE/Yelp_different_K"
 ### calculate acc 
 # bash scripts/hf/yelp/downstream.sh
 num_train_epochs=5
-for seed in 0 1 2 3
+for seed in 0 1 2
 do
 for label in "label1" "label2"
 do
