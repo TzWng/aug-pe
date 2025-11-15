af63f6576382438a97d43cba7ae2a1116a304c6e
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-14 20:40:12 -0500

    Update downstream.sh

--- scripts/hf/yelp/downstream.sh
+++ scripts/hf/yelp/downstream.sh
@@ -13,7 +13,7 @@ for  (( iter=${num_train_epochs}; iter>=0; iter-- ))
 do
 for privacy in 0
 do
-for method in "secpe"
+for method in "secpe600"
 do
 train_file="${result_folder}/${method}_${privacy}_prefixed.csv"
 if [ -e "$train_file" ]; then
